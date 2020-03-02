704ab52e8fee3694c7ce911759bf97a19b3964fe
T.I. Guido <coordti@lojasguido.com.br>
2020-03-02 15:24:34 -0300

    Add

---
+++ AulaX1/AulaX1/Services/ViaCEPServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Aulas.Services.Model;
+using Newtonsoft.Json;
+
+namespace Aulas.Services
+{
+    public class ViaCEPServices
+    {
+        private static string endURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BrowserAdressViaCEP(String cep)
+        {
+            string NewAressURL = string.Format(endURL, cep);
+
+            WebClient wc = new WebClient();
+            string content = wc.DownloadString(NewAressURL);
+
+            Endereco address = JsonConvert.DeserializeObject<Endereco>(content);
+
+            return address;
+
+        }
+
+
+    }
+}
