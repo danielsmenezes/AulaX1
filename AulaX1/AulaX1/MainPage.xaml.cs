aedb80806ab698d959206887b3af8d56092cb2ba
T.I. Guido <coordti@lojasguido.com.br>
2020-03-02 14:01:51 -0300

    App01 Ok

--- AulaX1/AulaX1/MainPage.xaml.cs
+++ AulaX1/AulaX1/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AulaX1.Services;
+using AulaX1.Services.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -16,6 +18,34 @@ namespace AulaX1
         public MainPage()
         {
             InitializeComponent();
+
+            btnSearch.Clicked += SearchCEP;
+
+        }
+
+        private void SearchCEP(object sender, EventArgs args)
+        {
+
+            if (isValidCEP(tbCEP.Text.Trim()))
+            {
+                Endereco end = ViaCEPServices.BrowserAdressViaCEP(tbCEP.Text.Trim());
+
+                lblResult.Text = string.Format("Endereço: {0}, {1} {2}, {3}", end.logradouro, end.bairro, end.localidade, end.uf);
+            }
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            if(cep.Length != 8)
+            {
+                DisplayAlert("Erro","CEP Inválido", "OK");
+            }
+            else
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
